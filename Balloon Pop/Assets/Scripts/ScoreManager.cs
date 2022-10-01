b89db148b803da5c23a2a0dfadf880376a4c5028
Kimberly Grant <10945830@uvu.edu>
2022-09-30 21:33:58 -0600

    Balloon Pop

--- Balloon Pop/Assets/Scripts/ScoreManager.cs
+++ Balloon Pop/Assets/Scripts/ScoreManager.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    public int score;
+    public TextMeshProUGUI scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateScoreText();
     }
 
     // Update is called once per frame
-    void Update()
+    public void IncreaseScoreText(int amount)
+    {
+        score += amount;
+        UpdateScoreText();
+    }
+
+    public void DecreaseScoreText(int amount)
     {
         
     }
